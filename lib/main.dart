e68db6c46abfa1802fb77eb36d724ff4efe8ad9f
712xxxabd-cell <712xxxabd@gmail.com>
2026-09-18 20:08:00 +0300

    Rename RizqAiApp to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const RizqAiApp());
+void main() => runApp(const MyApp());
 
-class RizqAiApp extends StatelessWidget {
-  const RizqAiApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
